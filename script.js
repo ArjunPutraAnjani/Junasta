917e6f4d3446f73bd40c7008300e929cd12e5a37
ArjunPutraAnjani <arjunputraanjani13@gmail.com>
2025-04-11 20:49:10 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
     navbarNav.classList.toggle("active");
 };
+
+// KLICK ON OUT SIDE SIDEBAR FOR REMOVE FUNGTION
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener("click" ,function(e) {
+ if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  navbarNav.classList.remove("active");
+ }
+});
